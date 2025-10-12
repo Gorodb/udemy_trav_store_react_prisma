d0e93c2289fee6b833092812968b182973e40e75
Ramis Vakazov <rvakazov@gmail.com>
2025-10-12 17:38:30 +0200

    added cards with dummy data for them

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import {Metadata} from "next";
-
-export const metadata: Metadata = {
-  title: "Home"
-}
+import {sampleData} from "@/db/sample-data"
+import {ProductList} from "@/components/shared/product/Product-list";
 
 export default function Homepage() {
-  return (<>Pro store</>)
+  return (
+    <>
+      <ProductList data={sampleData} title="Newest Arrivals" limit={4} />
+    </>
+  )
 }
