0601020866e70343acf87a6f71ca8e19113f10df
Ramis Vakazov <rvakazov@gmail.com>
2025-10-12 16:42:49 +0200

    updated header layout

---
+++ components/shared/mode-toggle/ModeToggle.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import {useState, useEffect} from "react";
+import {useTheme} from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuContent,
+  DropdownMenuCheckboxItem
+} from "@/components/ui/dropdown-menu";
+import {Button} from "@/components/ui/button";
+import {SunIcon, MoonIcon, SunMoon} from 'lucide-react';
+
+export function ModeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const {theme, setTheme} = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className='focus-visible:ring-0 focus-visible:ring-offset-0 cursor-pointer'>
+          {theme === 'system' ? (
+            <SunMoon/>
+          ) : theme === 'dark' ? (
+            <MoonIcon/>
+          ) : <SunIcon/>}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator/>
+        <DropdownMenuCheckboxItem checked={theme === 'system'} onClick={() => setTheme('system')} className='cursor-pointer'>
+          System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === 'light'} onClick={() => setTheme('light')} className='cursor-pointer'>
+          Light
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === 'dark'} onClick={() => setTheme('dark')} className='cursor-pointer'>
+          Dark
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
